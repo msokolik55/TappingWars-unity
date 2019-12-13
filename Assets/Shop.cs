8b192195caaf54bbeae9b5be16831314ee7f7210
PatrikSehnoutek <58843354+PatrikSehnoutek@users.noreply.github.com>
2019-12-13 17:30:58 +0100

    Basic UI

---
+++ Assets/Shop.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    Animator _animator;
+    public GameObject arrow;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    
+    public void OpenOrClose()
+    {
+        if (_animator.GetBool("open"))
+        {
+            _animator.SetBool("open", false);
+            arrow.SetActive(true);
+        }
+        else
+        {
+            _animator.SetBool("open", true);
+            arrow.SetActive(false);
+        }
+    }
+}
