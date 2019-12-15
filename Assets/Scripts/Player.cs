ae3ee65f7bc7536b3d1831083dfe446d7630f4f5
PatrikSehnoutek <58843354+PatrikSehnoutek@users.noreply.github.com>
2019-12-15 13:25:41 +0100

    Player Prefab

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class Player
+{
+    float health;
+    string name;
+    int money;
+    float damage;
+
+    public Player(float _health, string _name, int _money, float _damage)
+    {
+        health = _health;
+        name = _name;
+        money = _money;
+        damage = _damage;
+    }
+
+}
