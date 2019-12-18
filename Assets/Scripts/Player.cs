1aadaafdcc038fffb48d03e0f6a70f190d856949
PatrikSehnoutek <58843354+PatrikSehnoutek@users.noreply.github.com>
2019-12-18 22:42:06 +0100

    UI

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+class Player : MonoBehaviour
+{
+    float health = 100f;
+    string name;
+    int money = 0;
+    float damage = 1f;
+
+    public Image healthBar; 
+    
+
+    public Player(float _health, string _name, int _money, float _damage)
+    {
+        health = _health;
+        name = _name;
+        money = _money;
+        damage = _damage;
+
+        healthBar.fillAmount = health / 100f;    // priradit to funkcie takeDamage
+    }
+
+}
