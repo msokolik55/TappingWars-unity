f7b1ec5315032848bef4b5fc436eda4c72cc2db9
PatrikSehnoutek <58843354+PatrikSehnoutek@users.noreply.github.com>
2019-12-19 18:06:33 +0100

    SinglePlayer new functions

---
+++ Assets/Scripts/Items.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Items : MonoBehaviour
+{
+    Player playerScript;
+    public GameObject playerUnit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerScript = playerUnit.GetComponent<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void RepairTool()
+    {
+        playerScript.Repair();
+    }
+
+    public void DamageTool()
+    {
+        playerScript.IncreaseDamage();
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,16 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Networking;
 
-class Player : MonoBehaviour
+public class Player : NetworkBehaviour
 {
     float health = 100f;
     string name;
     int money = 0;
     float damage = 1f;
 
-    public Image healthBar; 
-    
+    public Image healthBar;
+    public Text numberOfCoins;
+
 
     public Player(float _health, string _name, int _money, float _damage)
     {
@@ -21,6 +23,42 @@ class Player : MonoBehaviour
         damage = _damage;
 
         healthBar.fillAmount = health / 100f;    // priradit to funkcie takeDamage
+    }
+
+    public void TakeDamage()
+    {
+        healthBar.fillAmount = health / 100f;
+    }
+
+    public void EarnMoney()
+    {
+        money += 5;
+        numberOfCoins.text = money.ToString();
+    }
+
+    public void Repair()
+    {
+        if (health + 10f > 100)
+        {
+            health = 100f;
+        }else
+        {
+            health += 10f;
+        }
+        healthBar.fillAmount = health / 100f; 
+    }
+
+    public void IncreaseDamage()
+    {
+        if(money >= 20)
+        {
+            money -= 20;
+            numberOfCoins.text = money.ToString(); 
+        }
+        else
+        {
+            // vypisat ze ma malo penazi
+        }
     }
 
 }
