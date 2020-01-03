63c119f31e6b5f6c224305afb280fa090dd4fb38
PatrikSehnoutek <58843354+PatrikSehnoutek@users.noreply.github.com>
2020-01-03 12:57:03 +0100

    Timer

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -8,6 +8,10 @@ public class Timer : MonoBehaviour
     public float startTime;
     public Text textBox;
 
+    private string seconds;
+    private string minutes;
+    private int time = 120;
+
     public static bool timeOver = false;
 
     // Start is called before the first frame update
@@ -17,10 +21,27 @@ public class Timer : MonoBehaviour
 
     }
 
+    private void DisplayTime()
+    {
+
+    }
+
     // Update is called once per frame
     void Update()
     {
-        startTime += Time.deltaTime;
-        textBox.text = (startTime/60f).ToString("f2");
+
+        if (time == 0)
+        {
+            timeOver = true;
+        }
+        else
+        {
+            startTime += Time.deltaTime;
+            time = 120 - ((int) Mathf.Round(startTime * 100) / 100);
+            minutes = "0" + (time / 60).ToString();
+            seconds = ((time % 60) < 10) ? "0"+(time % 60).ToString() : (time % 60).ToString();
+
+            textBox.text = minutes + ":" + seconds;
+        }
     }
 }
