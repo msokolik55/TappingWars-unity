167dca7057701d59190c2642278f2eff4ed0d8a9
PatrikSehnoutek <58843354+PatrikSehnoutek@users.noreply.github.com>
2020-01-21 22:42:07 +0100

    Pato Main Menu

---
+++ Assets/PatoMenu/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject profile;
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Profile()
+    {
+        profile.GetComponent<Animator>().SetBool("open", !profile.GetComponent<Animator>().GetBool("open"));
+    }
+}
